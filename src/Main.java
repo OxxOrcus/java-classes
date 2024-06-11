839e29441b3d8c16c503af8be8347968c173b9f2
Dev_Oxx.Orcus <paulobroccooxx@gmail.com>
2024-06-11 21:51:07 +0100

    classes 3 and 4

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,84 @@
 public class Main {
     public static void main(String[] args) {
-//! FIRST CLASS (STRING)
-    //    String nome = "Paulo";
-    //    System.out.println("Hello, " + nome + "!");
+        // ! FIRST CLASS (STRING)
+        // String nome = "Paulo";
+        // System.out.println("Hello, " + nome + "!");
+        // -------------------------------------------------------------------------------------------------------
+        // ! SECOND CLASS (NUMBERS) INT float
+        // int a;
+        // int b = 2;
+        // a = 3;
+        // int soma = a + b;
+        // int subtracao = a - b;
+        // int multiplicacao = a * b;
+        // float divisao = (float)a / b;
+        //
+        // System.out.println(soma);
+        // System.out.println(subtracao);
+        // System.out.println(multiplicacao);
+        // System.out.println(divisao);
+        // -------------------------------------------------------------------------------------------------------
+        // ! THIRD CLASS (BOOLEAN)
+        // boolean a = true;
+        // boolean b = false;
+        // System.out.println(a);
+        // System.out.println(b);
 
-//! SECOND CLASS (NUMBERS) INT
-        int a;
-        int b = 2;
-        a = 3;
-        int soma = a + b;
-        int subtracao = a - b;
-        int multiplicacao = a * b;
-        int divisao = a / b;
+        // Tabela Verdade
+        // Operador && (AND)
+        // true & true = true
+        // true & false = false
+        // false & true = false
+        // false & false = false
 
-        System.out.println(soma);
-        System.out.println(subtracao);
-        System.out.println(multiplicacao);
-        System.out.println(divisao);
+        // Operador || (OR)
+        // true || true = true
+        // true || false = true
+        // false || true = true
+        // false || false = false
+
+        // ---------------------------------------------------------------------------------------------------
+
+        // ! FOURTH CLASS (ESTRUTURAS CONDICIONAIS)
+        // int nota = 80;
+        // String graduacao;
+
+        // nota for maior que 70 APROVADO
+        // if-else
+        // if (nota >= 70) {
+        // System.out.println("Aluno Aprovado!");
+        // } else {
+        // System.out.println("Aluno Reprovado");
+        // }
+
+        // simulacao de regras das classificacoes de notas: A 80 B 70 C 60 D 0
+        // if (nota >= 80) {
+        // graduacao = "A";
+        // } else if (nota < 80 && nota>=70) {
+        // graduacao = "B";
+        // } else if (nota < 70 && nota>=60) {
+        // graduacao = "C";
+        // } else if (nota < 60 && nota>=0) {
+        // graduacao = "D";
+        // } else {
+        // graduacao = "";
+        // }
+        //
+        // switch (graduacao) {
+        // case "A":
+        // case "B":
+        // System.out.println("Aluno aprovado!");
+        // break;
+        // case "C":
+        // case "D":
+        // System.out.println("Aluno Reprovado");
+        // break;
+        // default:
+        // System.out.println("Graduacao invalida!");
+        // }
+
+        // ---------------------------------------------------------------------------------------------------
+        // ! CLASS 5 (MANIPULACAO DE STRINGS E SATAS)
+
     }
 }
