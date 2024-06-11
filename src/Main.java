92fa1c3bc32fb5943591f08c440b1ac8e3d2ce34
Dev_Oxx.Orcus <paulobroccooxx@gmail.com>
2024-06-11 18:54:57 +0100

    operadores sem abreviacao

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,13 @@ public class Main {
         int b = 2;
         a = 3;
         int soma = a + b;
-        
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        int divisao = a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
     }
 }
