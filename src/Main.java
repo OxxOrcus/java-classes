8e85d725c6cca287ea36205806f405ea26578071
Dev_Oxx.Orcus <paulobroccooxx@gmail.com>
2024-06-11 18:47:29 +0100

    second class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+//! FIRST CLASS (STRING)
+    //    String nome = "Paulo";
+    //    System.out.println("Hello, " + nome + "!");
 
-        String nome = "Paulo";
-        System.out.println("Hello, " + nome + "!");
+//! SECOND CLASS (NUMBERS) INT
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        
     }
 }
