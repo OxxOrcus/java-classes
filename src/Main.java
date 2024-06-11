8aa120d7bf2da4225167033d0eb55e37eb6fbd40
Dev_Oxx.Orcus <paulobroccooxx@gmail.com>
2024-06-11 21:52:10 +0100

    restructure comments

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         // ! FIRST CLASS (STRING)
         // String nome = "Paulo";
         // System.out.println("Hello, " + nome + "!");
-        // -------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
         // ! SECOND CLASS (NUMBERS) INT float
         // int a;
         // int b = 2;
@@ -17,68 +17,73 @@ public class Main {
         // System.out.println(subtracao);
         // System.out.println(multiplicacao);
         // System.out.println(divisao);
-        // -------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------
         // ! THIRD CLASS (BOOLEAN)
-        // boolean a = true;
-        // boolean b = false;
-        // System.out.println(a);
-        // System.out.println(b);
+//        boolean a = true;
+//        boolean b = false;
+//         System.out.println(a);
+//         System.out.println(b);
 
-        // Tabela Verdade
-        // Operador && (AND)
-        // true & true = true
-        // true & false = false
-        // false & true = false
-        // false & false = false
+//         Tabela Verdade
+        //Operador && (AND)
+        //true & true = true
+        //true & false = false
+        //false & true = false
+        //false & false = false
 
-        // Operador || (OR)
-        // true || true = true
-        // true || false = true
-        // false || true = true
-        // false || false = false
+        //Operador || (OR)
+        //true || true = true
+        //true || false = true
+        //false || true = true
+        //false || false = false
 
-        // ---------------------------------------------------------------------------------------------------
 
-        // ! FOURTH CLASS (ESTRUTURAS CONDICIONAIS)
-        // int nota = 80;
-        // String graduacao;
+//---------------------------------------------------------------------------------------------------
+
+
+// FOURTH CLASS (ESTRUTURAS CONDICIONAIS)
+//        int nota = 80;
+//        String graduacao;
 
         // nota for maior que 70 APROVADO
         // if-else
-        // if (nota >= 70) {
-        // System.out.println("Aluno Aprovado!");
-        // } else {
-        // System.out.println("Aluno Reprovado");
-        // }
+//        if (nota >= 70) {
+//            System.out.println("Aluno Aprovado!");
+//        } else {
+//            System.out.println("Aluno Reprovado");
+//        }
+
 
         // simulacao de regras das classificacoes de notas: A 80 B 70 C 60 D 0
-        // if (nota >= 80) {
-        // graduacao = "A";
-        // } else if (nota < 80 && nota>=70) {
-        // graduacao = "B";
-        // } else if (nota < 70 && nota>=60) {
-        // graduacao = "C";
-        // } else if (nota < 60 && nota>=0) {
-        // graduacao = "D";
-        // } else {
-        // graduacao = "";
-        // }
-        //
-        // switch (graduacao) {
-        // case "A":
-        // case "B":
-        // System.out.println("Aluno aprovado!");
-        // break;
-        // case "C":
-        // case "D":
-        // System.out.println("Aluno Reprovado");
-        // break;
-        // default:
-        // System.out.println("Graduacao invalida!");
-        // }
+//        if (nota >= 80) {
+//            graduacao = "A";
+//        } else if (nota < 80 && nota>=70) {
+//            graduacao = "B";
+//        } else if (nota < 70 && nota>=60) {
+//            graduacao = "C";
+//        } else if (nota < 60 && nota>=0) {
+//            graduacao = "D";
+//        } else {
+//            graduacao = "";
+//        }
+//
+//        switch (graduacao) {
+//            case "A":
+//            case "B":
+//                System.out.println("Aluno aprovado!");
+//                break;
+//            case "C":
+//            case "D":
+//                System.out.println("Aluno Reprovado");
+//                break;
+//            default:
+//                System.out.println("Graduacao invalida!");
+//        }
 
-        // ---------------------------------------------------------------------------------------------------
-        // ! CLASS 5 (MANIPULACAO DE STRINGS E SATAS)
+
+//---------------------------------------------------------------------------------------------------
+// CLASS 5 (MANIPULACAO DE STRINGS E SATAS)
+        
 
     }
 }
