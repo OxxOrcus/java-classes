c74fe522e79af90c74d62ca3c5575046e63eea37
Dev_Oxx.Orcus <paulobroccooxx@gmail.com>
2024-06-13 01:05:11 +0100

    Last class of firt moduel

--- src/Main.java
+++ src/Main.java
@@ -111,12 +111,45 @@ public class Main {
 
             // 1 2 3 4 5 6 7 8 9 10
             // para uma variavel que inicia em 1, e ai ate 10, mudando 1-por-1, faca:
-            for (int i = 1; i <= 10; i++) {
-                for (int j = 1; j <= 10; j++) {
-                    System.out.println(j + "x" + i + "=" + j * i);
-                    //1x1=1
-                    //2x1=2
-                }
-            }
-        }
+//            for (int i = 1; i <= 10; i++) {
+//                for (int j = 1; j <= 10; j++) {
+//                    System.out.println(j + "x" + i + "=" + j * i);
+//                    //1x1=1
+//                    //2x1=2
+//                }
+//            }
+            // ---------------------------------------------------------------------------------------------------
+//! CLASS 7 (VETORES)
+
+//            int numero1 = 1;
+//            int numero2 = 2;
+//            int numero3 = 3;
+//            int numero4 = 4;
+//            int numero5 = 5;
+//
+//            int [] numeros = new int[5];
+//            //[0] [1] [2] [3] [4]
+//            numeros[0] = 1;
+//            numeros[1] = 2;
+//            numeros[2] = 3;
+//            numeros[3] = 4;
+//            numeros[4] = 5;
+//            for (int i=0; i < numeros.length; i++) {
+//                System.out.println(numeros[i]);
+////            }
+//            String[] letras = { "A", "B", "C", "J", "x"};
+////            for (int i=0; i < letras.length; i++);
+////                System.out.println(letras[i]);
+//
+//            System.out.println(Arrays.toString(letras));
+
+            // ---------------------------------------------------------------------------------------------------
+//! CLASS 8 (FUNCOES)
+        String nome = "Paulo";
+        saudacao(nome);
+       }
+
+       public static void saudacao(String nome) {
+          System.out.println("Hello, " + nome);
+       }
 }
